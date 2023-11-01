fc9ed934648bc8dc3a9ca199325d1baf0fba28a9
Ruslan Seferov <91841957+ruslansfrov236@users.noreply.github.com>
2023-11-01 15:46:00 +0400

    Program.cs

--- Task-3/Task-3/Program.cs
+++ Task-3/Task-3/Program.cs
@@ -4,3 +4,15 @@ for(int i=adSoyad.Length-1; i>=0;i--)
 {
     Console.Write(adSoyad[i]);
 }
+
+  static string Tersi(string metin)
+{
+    char[] simvovlar = metin.ToCharArray();
+    Array.Reverse(simvovlar);
+    return new string(simvovlar);
+}
+Console.Write("Tam adinizi daxil edin :");
+string adSoyad = Console.ReadLine();
+string tersadSoyad = Tersi(adSoyad);
+Console.WriteLine(tersadSoyad);
+Console.WriteLine(tersadSoyad);
